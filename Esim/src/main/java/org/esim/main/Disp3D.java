538b73367315ad3db915085140517e5b57b64fdb
adrianisuru <adrianisuru@DESKTOP-P88LK4D>
2017-12-07 14:10:47 -0600

    blaj

--- Esim/src/main/java/org/esim/main/Disp3D.java
+++ Esim/src/main/java/org/esim/main/Disp3D.java
@@ -3,23 +3,46 @@ package org.esim.main;
 import org.esim.electrostatics.Field;
 import org.esim.engine.DisplayWindow;
 import org.esim.engine.GLFWWindow;
+import org.esim.engine.model.Model;
 import org.lwjgl.opengl.GL11;
 
 public class Disp3D extends DisplayWindow{
+	
+	Model graph;
+	float[] vertices;
+	
+	Field field;
+	
+	int res;
+	
 
-	public Disp3D(Field field, GLFWWindow window) {
+	public Disp3D(Field field, GLFWWindow window, int res) {
 		super(window);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void draw() {
+		
+		this.field = field;
+		this.res = res;
+		
+		vertices = new float[res*res];
+		
+		
+		//initialize graph
+		//graph = new ...
 		
 	}
 
 	@Override
+	public void draw() {
+		//draw graph
+		//graph.draw();
+		
+		//...
+		//maybe draw some other stuff
+	}
+
+	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		
+		//update graph
 		
 	}
 
